ded55aa548cf3b45a8ea714f9cb6673abb306e42
Bill Tsui <dhubilltsui@gmail.com>
2025-10-21 22:07:10 +0800

    :sparkles:提交一下

--- VarTest.java
+++ VarTest.java
@@ -1,8 +1,12 @@
 public class VarTest {
 
     public static void main(String[] args) {
-        //print the value of age.
+        // print the value of age.
         int age = 18;
         System.out.println("age = " + age);
+        System.out.println("----------------------------------------");
+
+        String name = "Bill";
+        System.out.println("name = " + name);
     }
 }
