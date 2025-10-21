3b6625770147589ac5a585b072a6ec7196f17f5d
Bill Tsui <dhubilltsui@gmail.com>
2025-10-21 21:56:20 +0800

    :tada:first commit

---
+++ VarTest.java
@@ -0,0 +1,8 @@
+public class VarTest {
+
+    public static void main(String[] args) {
+        //print the value of age.
+        int age = 18;
+        System.out.println("age = " + age);
+    }
+}
