3ca417a709807c5918f1f116fb15e50bb1b64090
Bill Tsui <dhubilltsui@gmail.com>
2025-11-04 22:03:12 +0800

    :sparkles:提交一下

--- src/mosfet/onjava21/fundamental/oop/NewPerson.java
+++ src/mosfet/onjava21/fundamental/oop/NewPerson.java
@@ -7,14 +7,34 @@
  * {@code @Date:}         Nov 04 2025 13:40
  * {@code @Description:}
  * --------------------------------------------------------------------------
+ * ********************执行次序********************
+ * 父类静态代码块
+ * 子类静态代码块
+ * 父类构造代码块
+ * 父类构造方法体
+ * 子类构造代码块
+ * 子类构造方法体
  */
 package mosfet.onjava21.fundamental.oop;
 
-public class NewPerson {
+//静态导入
+
+import static java.lang.System.out;
+
+/**
+ * finale修饰的类不能被inherit
+ */
+public /*final*/ class NewPerson {
     private String name;
     private short age;
+    /*
+     * final修饰的变量必须被显式初始化，并且不能被修改
+     * 亦可在构造函数中初始化、构造代码块中初始化
+     */
+    private final String type = "Human";
 
     public NewPerson() {
+        out.printf("%d\n", 10);
         System.out.println("NewPerson() constructor called");
     }
 
@@ -51,7 +71,13 @@ public class NewPerson {
         System.out.println("I am  eating " + food);
     }
 
-    public NewPerson returnParent(String name) {
+    /**
+     * final修饰的方法不能被override
+     *
+     * @param name
+     * @return
+     */
+    public /*final*/ NewPerson returnParent(String name) {
         return new NewPerson();
     }
 }
