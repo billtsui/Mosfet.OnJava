a524b1a0ee7f581f9b2d90824ce5a9b2f693d2c9
Bill Tsui <dhubilltsui@gmail.com>
2025-11-24 15:49:23 +0800

    ✨ 提交一下

---
+++ src/main/java/person/billtsui/onjava/fundamental/oop/records/Point.java
@@ -0,0 +1,14 @@
+/**
+ * @author         Bill Tsui
+ * @date           Nov 24 2025
+ * @version        1.0
+ * @description 
+ * 
+ * Copyright (c)  Bill Tsui. All rights reserved.
+ * Licensed under the GPLv3 License. 
+ */
+
+package person.billtsui.onjava.fundamental.oop.records;
+
+public record Point(int x, int y) {
+}
