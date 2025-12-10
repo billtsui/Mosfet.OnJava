84841d055b27271d1a5b838a483df7e9b02e9a3f
Bill Tsui <dhubilltsui@gmail.com>
2025-12-10 15:14:37 +0800

    ✨ Record

---
+++ src/main/java/person/billtsui/onjava/fundamental/oop/records/State.java
@@ -0,0 +1,28 @@
+/**
+ * @author         Bill Tsui
+ * @date           Dec 10 2025
+ * @version        1.0
+ * @description 
+ * 
+ * Copyright (c)  Bill Tsui. All rights reserved.
+ * Licensed under the GPLv3 License. 
+ */
+
+package person.billtsui.onjava.fundamental.oop.records;
+
+import java.util.List;
+
+public record State(String name, String capitalCity, List<String> cities) {
+
+    public State   {
+        cities = List.copyOf(cities);
+    }
+
+    public State(String name, String capitalCity) {
+        this(name, capitalCity, List.of());
+    }
+
+    public State(String name, String capitalCity, String... cities) {
+        this(name, capitalCity, List.of(cities));
+    }
+}
